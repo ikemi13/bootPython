9fc7252a4fe73c027a4808d7a1daf2dfde4215b7
ikemi <home@FuertesMac.local>
2019-01-09 19:50:55 +0100

    Funciones Clásicas

---
+++ p1.py
@@ -0,0 +1,16 @@
+def  sumaTodos ( limitTo ):
+    resultado =  0
+    para i en el  rango ( 0 , limitTo + 1 ):
+        resultado + = i
+        
+    retorno resultado
+
+def  sumaTodosLosCuadrados ( limitTo ):
+    resultado =  0
+    para i en el  rango (limitTo + 1 ):
+        resultado + = i * i
+        
+    retorno resultado
+
+imprimir (sumaTodos ( 100 ))
+Imprimir (sumaTodosLosCuadrados ( 3 ))
